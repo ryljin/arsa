629c7889d6e4ab71ecd4777ae923b21dbd651326
sjp9 <sjp9482@nyu.edu>
2025-10-06 14:56:09 -0400

    Add CRUD API endpoints for cities to endpoints.py

--- server/endpoints.py
+++ server/endpoints.py
@@ -7,6 +7,7 @@ The endpoint called `endpoints` will return all available endpoints.
 from flask import Flask  # , request
 from flask_restx import Resource, Api  # , fields  # Namespace
 from flask_cors import CORS
+from server import cities
 
 # import werkzeug.exceptions as wz
 
@@ -46,3 +47,43 @@ class Endpoints(Resource):
         """
         endpoints = sorted(rule.rule for rule in api.app.url_map.iter_rules())
         return {"Available endpoints": endpoints}
+
+city_model = api.model('City', {'name': fields.String(required=True, description='City Name')})
+
+@api.route('/cities')
+class CityList(Resource):
+    @api.doc('list_cities')
+    def get(self):
+        return [{'id': cid, **data} for cid, data in cities.cities.items()]
+
+    @api.expect(city_model)
+    @api.doc('create_city')
+    def post(self):
+        data = request.json
+        city_id = cities.create(data)
+        return {'id': city_id, **data}, 201
+
+@api.route('/cities/<string:city_id>')
+class City(Resource):
+    @api.doc('get_city')
+    def get(self, city_id):
+        city = cities.cities.get(city_id)
+        if not city:
+            api.abort(404, "City not found")
+        return {'id': city_id, **city}
+
+    @api.expect(city_model)
+    @api.doc('update_city')
+    def put(self, city_id):
+        if city_id not in cities.cities:
+            api.abort(404, "City not found")
+        data = request.json
+        cities.cities[city_id] = data
+        return {'id': city_id, **data}
+
+    @api.doc('delete_city')
+    def delete(self, city_id):
+        if city_id not in cities.cities:
+            api.abort(404, "City not found")
+        del cities.cities[city_id]
+        return '', 204
